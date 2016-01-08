48e8cf4be5a6195bac3baa6a07eec56c64ce8c4e
gdutxiaoxu <973243764@qq.com>
2016-01-08 22:16:09 +0800

    第一次提交代码

---
+++ src/com/bjsxt/tank/Direction.java
@@ -0,0 +1,14 @@
+package com.bjsxt.tank;
+
+/**
+ * 
+
+ * @Description:改枚举类是用来定义八个方向的
+
+ * @author:徐俊
+
+ * @time:2016年1月8日 下午9:42:08
+ */
+public enum Direction {
+	L, LU, U, RU, R, RD, D, LD, STOP
+}
